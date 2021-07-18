2d7a29dd13a60a3185e4d2fc0e71e9b4944ce321
Mitansh Patil <smitanshpatil@gmail.com>
2021-07-18 10:59:04 +0530

    My new website

---
+++ deploy.sh
@@ -0,0 +1,7 @@
+export JEKYLL_ENV=production
+JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production source _scripts/sh/create_pages.sh
+JEKYLL_ENV=production jekyll build
+JEKYLL_ENV=production tools/build.sh -d ../mitanshp.github.io/
+
+
